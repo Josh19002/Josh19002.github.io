10f22503c7552d4e27d5b04f5577cce5bbb18472
Josh19002 <josh123478@gmail.com>
2022-04-27 16:38:11 -0600

    Remove logs from JS

--- weather/javascript/all-pages.js
+++ weather/javascript/all-pages.js
@@ -1,11 +1,8 @@
 "use strict";
 var current_date = new Date();
 var year = current_date.getFullYear()
-console.log(year)
 var month = current_date.getDate()
-console.log(month)
 var day = current_date.getDay()
-console.log(day)
 var date_pretty =new Date().toDateString()
 document.getElementById("cur_date").innerText = date_pretty
 document.getElementById("current_year").innerText= current_date.getFullYear();
