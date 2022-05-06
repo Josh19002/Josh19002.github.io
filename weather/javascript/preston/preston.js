a9e7d456d382342f6b1bb1787e3b8eb2a7d3ba60
Josh19002 <josh123478@gmail.com>
2022-05-06 17:28:43 -0600

    Fix images to match standard. Add windchill humidity and speed

--- weather/javascript/preston/preston.js
+++ weather/javascript/preston/preston.js
@@ -1,1 +1,11 @@
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","weatherwidget-io-js");
+async function getWeather() {
+    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?id=5604473&exclude=sys,timezone&units=imperial&appid=b0d07380ce5ea2770c47bb85b41d4544");
+    let data = await response.json();
+    let windchill = 35.74 + (0.6215*data.main.temp) - (35.75*(data.wind.speed**0.16)) + (0.4275*data.main.temp*(data.wind.speed**0.16))
+    document.getElementById("wind-speed").innerText = "Wind Speed: " + data.wind.speed + " MPH"
+    document.getElementById("humidity").innerText = "Humidity: " + data.main.humidity + "%"
+    document.getElementById("wind-chill").innerText = "Wind Chill: " + Math.round(windchill) + "\u00B0 F"
+}
+getWeather()
+
